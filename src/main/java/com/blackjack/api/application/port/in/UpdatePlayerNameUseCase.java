58b269a902a3f4882108986820f825f5002d260b
Federico Cantore <f.cantore85@gmail.com>
2026-04-07 19:39:59 +0200

    fix: delete UpdatePlayerNameCase port

--- src/main/java/com/blackjack/api/application/port/in/UpdatePlayerNameUseCase.java
+++
@@ -1,9 +0,0 @@
-package com.blackjack.api.application.port.in;
-
-import com.blackjack.api.application.dto.command.UpdatePlayerNameCommand;
-import com.blackjack.api.application.dto.response.PlayerResponse;
-import reactor.core.publisher.Mono;
-
-public interface UpdatePlayerNameUseCase {
-    Mono<PlayerResponse> execute(UpdatePlayerNameCommand command);
-}
