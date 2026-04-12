27d8ab3bb567e63d847b71f453dc24366341f6bb
Federico Cantore <f.cantore85@gmail.com>
2026-04-12 19:59:19 +0200

    fix: refactor exception extends ApplicationException

--- src/main/java/com/blackjack/api/domain/exception/GameNotFoundException.java
+++ src/main/java/com/blackjack/api/domain/exception/GameNotFoundException.java
@@ -2,11 +2,13 @@ package com.blackjack.api.domain.exception;
 
 import com.blackjack.api.domain.valueobject.GameId;
 
-public class GameNotFoundException extends RuntimeException {
+public class GameNotFoundException extends ApplicationException {
+    public static final String ERROR_CODE = "GAME_NOT_FOUND";
+
     public GameNotFoundException(GameId gameId) {
-        super("Game with ID: " + gameId + " not found");
+        super("Game with ID: " + gameId + " not found", ERROR_CODE);
     }
     public GameNotFoundException(String message) {
-        super(message);
+        super(message, ERROR_CODE);
     }
 }
